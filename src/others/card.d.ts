5d943f48dc162caa2aa9b91e08c04768680a3621
caohuichang <satrong@foxmail.com>
2023-05-08 03:19:03 +0000

    fix: compatible string type

--- src/others/card.d.ts
+++ src/others/card.d.ts
@@ -7,7 +7,7 @@ export type ElCard = TComponent<{
   /** 设置 header，也可以通过 slot#header 传入 DOM */
   header?: string
   /** 设置 body 的样式，默认值 `{ padding: '20px' }` */
-  bodyStyle?: Record<string, any>
+  bodyStyle?: Record<string, any> | string
   /** 设置阴影显示时机，默认值 `always` */
   shadow?: 'always' | 'hover' | 'never'
 }>
